edbed3aeb042ddf8fab192acde4fcd1979f19ec2
Inon Sean <inon_sean@gmail.com>
2021-12-28 19:48:31 +0200

    update

--- client.py
+++ client.py
@@ -3,14 +3,22 @@ class client:
     def __init__(self, team_name,TEST):
         self.tema_name=team_name
         self.gameClientUDP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-        if TEST:
-            self.gameClientUDP.bind(('172.99.255.255', 13117))
-        else:
-            self.gameClientUDP.bind(('172.1.255.255', 13117))
+        self.gameClientUDP.bind(("", 13117))
         self.gameClientTCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        print(" Server started, listening onIP address 172.1.0.88”")
+        print("Client started, listening for offer requests...")
+    def looking_server(self):
+        self.gameClientUDP.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+        self.gameClientUDP.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        while True:
+            data, addr = self.gameClientUDP.recvfrom(1024)
+            
+            
+
 if __name__ == '__main__':
-    client("the good fellas",False);
+    c=client("the good fellas",False)
+    c.looking_server()
+
+
 
 
 
