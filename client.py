3c04779e42a83d6e3c2558459d4b31cb6786d02d
Inon Sean <inon_sean@gmail.com>
2021-12-28 23:10:50 +0200

    udp finish?

--- client.py
+++ client.py
@@ -7,18 +7,29 @@ class client:
         self.gameClientUDP.bind(("", 13117))
         self.gameClientTCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         print("Client started, listening for offer requests...")
+        self.true_magic_cookie= 0xabcddcba
+        self.true_message_type= 0x2
     def looking_server(self):
         self.gameClientUDP.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
         self.gameClientUDP.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         while True:
-            data, addr = self.gameClientUDP.recvfrom(1024)
-            magic_cookie,message_type,server_port= struct.unpack('!IbH',data)
-            if(server_port == 2121):
+            try:
+                data, addr = self.gameClientUDP.recvfrom(1024)
+            except:
+                pass
+            print(f"data: {len(data)}.")
+            magic_cookie,message_type,server_port = struct.unpack('IbH',data)
+            if server_port==2121:
                 print(magic_cookie)
                 print(message_type)
-                print(pork)
-                break;
-            print("again")
+                print(server_port)
+
+            # self.gameClientTCP.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+            # self.gameClientTCP.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+            # self.gameClientTCP.connect((addr[0],server_port))
+            # print(f"Connected to server {addr[0]}.") 
+            # self.gameClientTCP.sendall(str.encode("troling\n"))
+            
             
             
 
